dbb43d63cd55e750a59c701265e7ee173cae9e31
HumanM00n <mrouy92@gmail.com>
2024-04-08 09:45:28 +0200

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException; // Importer la classe ValidationException
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth/register');
+    }
+
+    public function registerSave(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email'=> 'required|email',
+            'password'=> 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+            'level' => 'Admin',
+        ]);
+
+        return redirect()->route('login');
+    }
+
+    public function login()
+    {
+        return view('auth/login');
+    }
+
+    public function loginAction(Request $request)
+    {
+        // Valider les données
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Tenter de s'authentifier
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed')
+            ]);
+        }
+
+        // Régénérer la session et rediriger vers le tableau de bord
+        $request->session()->regenerate();
+        return redirect()->route('dashboard');
+    }
+}
